a9582f4b04de54f94ce97370978a5ef6d9c44673
RealCashProject <36957906+RealCashProject@users.noreply.github.com>
2018-03-07 02:09:35 -0300

    update

--- website/config.js
+++ website/config.js
@@ -1,6 +1,6 @@
-var api = "http://127.0.0.1:8117";
+var api = "http://poolhost.com:8117";
 
-var poolHost = "192.210.228.189";
+var poolHost = "poolhost.com";
 
 var irc = "irc.freenode.net/#poolhost";
 
