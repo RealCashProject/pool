0a9fcffccf50ae0e52f520f379fc52b23c90c1a4
RealCashProject <36957906+RealCashProject@users.noreply.github.com>
2018-03-07 16:13:14 -0300

    update

--- website/config.js
+++ website/config.js
@@ -1,17 +1,17 @@
-var api = "http:/192.210.228.189:8805";
+var api = "http:/pool.realcash.tech:8805";
 
-var poolHost = "192.210.228.189";
+var poolHost = "http:/pool.realcash.tech:";
 
 var irc = "irc.freenode.net/#poolhost";
 
-var email = "support@poolhost.com";
+var email = "pool@realcash.tech";
 
 var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+var blockchainExplorer = "http://explorer.realcash.tech{symbol}/block/{id}";
 
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+var transactionExplorer = "http://explorer.realcash.tech/{symbol}/transaction/{id}";
 
 var themeCss = "themes/default-theme.css";
